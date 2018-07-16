d2392dd81b32c3505e00797b96037baa8a966492
Gibran Asshadiqi <asshadiqi.gibran@gmail.com>
2018-07-16 13:55:25 +0700

    mbuy

--- uaz gib/Jawaban UAS GIBRAN.R
+++ uaz gib/Jawaban UAS GIBRAN.R
@@ -8,4 +8,29 @@ library(RANN)
 library(readr)
 library(ROSE)
 library(mlbench)
+library(ggplot2)
+library(caret)
+library(psych)
+library(MVN)
+library(DescTools)
+library(biotools)
+library(corrplot)
+library(class)
+library(e1071)
+library(pROC)
+library(car)
+library(Hmisc)
+library(DMwR)
+library(rpart)
+library(GGally)
+library(CCA)
+library(CCP)
+library(vegan)
+
+##3D
+CountryData$GDPlevel = ifelse(CountryData$GDPcapita > 1500, 1, 0)
+
+
+
+
 
